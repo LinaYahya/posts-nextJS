17ae718499bf8eced1569dd2dc58449e95560523
Lina Ebeid <linaebe0@gmail.com>
2021-01-06 21:30:59 +0200

    render main page by SSR

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,60 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { useState, useEffect, useRef } from "react";
+import Head from "next/head";
+import Post from "../components/Post";
+import { fetchPosts } from "../store/actions";
+import store from "../store/store";
+import styles from "../styles/Home.module.css";
 
-export default function Home() {
+export default function Home({ posts }) {
+  const [endLimit, setEndLimit] = useState(10);
+  const [more, setMore] = useState(true);
+
+  const stateRef = useRef();
+  stateRef.current = endLimit;
+
+  const loadMoreHandler = () => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop !==
+      document.documentElement.offsetHeight
+    )
+      return;
+
+    if (stateRef.current === 100) {
+      setMore(false);
+    } else {
+      setEndLimit((prevEnd) => prevEnd + 10);
+    }
+  };
+
+  useEffect(() => {
+    if (more) {
+      window.addEventListener("scroll", loadMoreHandler);
+      return () => window.removeEventListener("scroll", loadMoreHandler);
+    }
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Posts App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      {console.log({ more, endLimit })}
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          {posts.slice(0, endLimit).map((post) => (
+            <Post post={post} key={post.id} />
+          ))}
         </div>
+        {!more && (<h3>no more data to load</h3>)}
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
-  )
+  );
+}
+
+export async function getServerSideProps() {
+  await store.dispatch(fetchPosts());
+  const { posts } = await store.getState();
+
+  return { props: { posts: posts.posts } };
 }
