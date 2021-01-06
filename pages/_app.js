290cae430e874f2c8e653a9b002242f99509c8bd
Lina Ebeid <linaebe0@gmail.com>
2021-01-06 19:21:44 +0200

    provide store at app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
+import { Provider } from 'react-redux';
+import { createWrapper } from 'next-redux-wrapper';
+import store from '../store/store';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
 
-export default MyApp
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  )
+}
+const makeStore = () => store;
+const wrapper = createWrapper(makeStore);
+
+export default wrapper.withRedux(MyApp);
