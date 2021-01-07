65027ff2283ff9e1ed216d330a24bb95e18b7fcf
Lina Ebeid <linaebe0@gmail.com>
2021-01-07 10:50:49 +0200

    create action to get post comments

--- store/actions.js
+++ store/actions.js
@@ -1,9 +1,13 @@
 // create async action to fetchPosts by return async function
 export const fetchPosts = () => async dispatch => {
-
   const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-
   const response = await res.json();
-
-  dispatch({ type: "FETCH_POSTS", payload: response });
+  dispatch({ type: 'FETCH_POSTS', payload: response });
 };
+
+// create async action to get comments on specific post by postID
+export const getPostComment = (id) =>  async dispatch => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/comments?postId=${id}`);
+  const comments = await res.json();
+  dispatch({ type: 'GET_POST_COMMENT', payload: id , comments})
+}
--- store/reducers.js
+++ store/reducers.js
@@ -4,6 +4,7 @@ const initialPostsState = {
   posts: [],
   error: null,
   loading: false,
+  post: null,
 }
 
 // create postsReducer function with action type FETCH_POSTS
@@ -15,6 +16,12 @@ const postsReducer = (state = initialPostsState, action) => {
         posts: action.payload,
         error: null,
         loading: false,
+      };
+    case 'GET_POST_COMMENT':
+      let post = state.posts.find(({id}) => id === Number(action.payload));
+      return {
+        ...state,
+        post: {...post, comments: action.comments}
       }
     default:
       return state
