19fd6e5202de5dd45e214a139a110705330fc8aa
Lina Ebeid <linaebe0@gmail.com>
2021-01-08 21:53:47 +0200

    add proptypes validation to comment component

--- components/Comment.js
+++ components/Comment.js
@@ -1,11 +1,13 @@
-import styles from '../styles/Post.module.css'
+import PropTypes from 'prop-types';
+import styles from '../styles/Post.module.css';
 
-export default function Comment({ comment }){
+export default function Comment({ comment }) {
   const { email, name, body } = comment;
+
   return (
     <li className={styles.comment}>
       <h4>{name}</h4>
-      <p style={{display: 'inline'}}>{body}</p>
+      <p style={{ display: 'inline' }}>{body}</p>
       <span>
         <a
           href={`mailto:${email}`}
@@ -14,9 +16,17 @@ export default function Comment({ comment }){
           target="_blank"
           rel="noreferrer"
         >
-          <i className="fas fa-envelope" style={{marginLeft: '90%'}}></i>
+          <i className="fas fa-envelope" style={{ marginLeft: '90%' }} />
         </a>
       </span>
     </li>
-  )
+  );
+}
+
+Comment.propTypes = {
+  comment: PropTypes.shape({
+    email: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    body: PropTypes.string.isRequired,
+  }).isRequired,
 };
