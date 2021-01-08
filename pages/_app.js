30675d475be2df3e3cb7d1cc7a18ec91e2a8700b
Lina Ebeid <linaebe0@gmail.com>
2021-01-08 16:57:07 +0200

    install nprogress module for ux at loading pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,18 @@
+import Router from 'next/router';
 import { Provider } from "react-redux";
 import { createWrapper } from "next-redux-wrapper";
 import Layout from "../components/Layout";
 import store from "../store/store";
+import NProgress from 'nprogress'; //nprogress module
+import 'nprogress/nprogress.css'; //styles of nprogress
 import "../styles/globals.css";
+
+
+//Binding events to display spinner when user navigate between routes 
+Router.events.on('routeChangeStart', () => NProgress.start()); 
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
+
 
 function MyApp({ Component, pageProps }) {
   return (
